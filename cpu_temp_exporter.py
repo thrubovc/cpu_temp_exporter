daab22c39b256febb823829f6c79a01d2f46d0d6
thrubovc <34124990+thrubovc@users.noreply.github.com>
2020-07-29 20:58:31 +0200

    Create cpu_temp_exporter.py

---
+++ cpu_temp_exporter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from prometheus_client import start_http_server, Gauge
+import os
+import time
+
+g = Gauge('cpu_temp', 'CPU Temperature')
+
+
+def process_request(t):
+    time.sleep(t)
+
+
+if __name__ == '__main__':
+    start_http_server(9111)
+    while True:
+        sensors = os.popen('/usr/bin/sensors|grep ^Tctl').readline()
+        temp = (sensors.replace('Tctl:         +', '').replace('°C', ''))
+        g.set(float(temp))
+        process_request(1)
